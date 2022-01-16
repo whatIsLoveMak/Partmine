2ac936d576a9af350d57f7d936208980b6d7a748
Vladislav <81952429+whatIsLoveMak@users.noreply.github.com>
2022-01-16 14:05:43 +0300

    Add files via upload

---
+++ new-parts-slider#dist/js/script.js
@@ -0,0 +1,53 @@
+// Инициализация превью слайдера
+const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { // ищем слайдер превью по селектору
+    // задаем параметры
+    direction: 'vertical', // вертикальная прокрутка
+    slidesPerView: 3, // показывать по 3 превью
+    spaceBetween: 24, // расстояние между слайдами
+    navigation: { // задаем кнопки навигации
+        nextEl: '.slider__next', // кнопка Next
+        prevEl: '.slider__prev' // кнопка Prev
+    },
+    freeMode: true, // при перетаскивании превью ведет себя как при скролле
+    breakpoints: { // условия для разных размеров окна браузера
+        0: { // при 0px и выше
+            direction: 'horizontal', // горизонтальная прокрутка
+        },
+        769: { // при 768px и выше
+            direction: 'vertical', // вертикальная прокрутка
+        }
+    }
+});
+// Инициализация слайдера изображений
+const sliderImages = new Swiper('.slider__images .swiper-container', { // ищем слайдер превью по селектору
+    // задаем параметры
+    direction: 'vertical', // вертикальная прокрутка
+    slidesPerView: 1, // показывать по 1 изображению
+    spaceBetween: 32, // расстояние между слайдами
+    mousewheel: true, // можно прокручивать изображения колёсиком мыши
+    navigation: { // задаем кнопки навигации
+        nextEl: '.slider__next', // кнопка Next
+        prevEl: '.slider__prev' // кнопка Prev
+    },
+    grabCursor: true, // менять иконку курсора
+    thumbs: { // указываем на превью слайдер
+        swiper: sliderThumbs // указываем имя превью слайдера
+    },
+    breakpoints: { // условия для разных размеров окна браузера
+        0: { // при 0px и выше
+            direction: 'horizontal', // горизонтальная прокрутка
+        },
+        769: { // при 768px и выше
+            direction: 'vertical', // вертикальная прокрутка
+        }
+    }
+});
+
+let blockText = document.querySelector('.parts__item-text');
+let height = document.querySelector('.slider__images .swiper-slide-active .slider__image');
+window.addEventListener('DOMContentLoaded', function() {
+    blockText.style.height = height.clientHeight + 'px';
+})
+window.addEventListener('resize', function() {
+    blockText.style.height = height.clientHeight + 'px';
+});
